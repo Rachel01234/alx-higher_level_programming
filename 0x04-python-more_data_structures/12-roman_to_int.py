5de12e504737eb8a5bf7a5f9a01bdeac9b95beb9
Rachel01234 <som63625@gmail.com>
2022-09-28 14:11:54 -0700

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,49 +1,57 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+
+def _get_value(char):
+    """
+    Returns the roman value of a character
+    None if its not a Roman Character
+    """
+    romans = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 100
+    }
+    char = char.upper()
+    if char in romans:
+        return romans[char]
+    return None
+
+
+def roman_to_int(roman):
+    """
+    Converts a roman numerals to Decimal
+    Args:
+        roman - the string f roman numerals
+    """
+
+    if not isinstance(roman, str) or roman is None:
         return 0
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    numbers = []
-    end_sum = 0
-    length = len(roman_string)
 
-    for i in range(0, length):
-        for k, v in roman.items():
-            if roman_string[i] == k:
-                numbers.append(v)
+    result, prev, cur = 0, 0, 0
 
-    current_sum = numbers[0]
+    for c in roman:
+        cur = _get_value(c)
+        if cur is None:
+            raise ValueError("Wrong input")
+        if cur > prev:
+            result -= prev
+            cur -= prev
+        result += cur
+        prev = cur
 
-    for i in range(1, len(numbers)):
-        if numbers[i - 1] == numbers[i]:
-            current_sum += numbers[i]
-        elif numbers[i - 1] > numbers[i]:
-            end_sum += current_sum
-            current_sum = numbers[i]
-        elif numbers[i - 1] < numbers[i]:
-            end_sum -= current_sum
-            current_sum = numbers[i]
-    return (current_sum + end_sum)
+    return result
 
-"""def roman_to_int(roman_string):
-    if type(roman_string) != str:
-        return 0
 
-    roman = {"I" : 1,"V" : 5,"X" : 10,"L" : 50,"C" : 100,"D" : 500,"M" : 1000}
-
-    r = 0
-    Prev_no = 0
-    curr_no = 0
-
-    for letter in (roman_string[::-1]):
-        prev_no = curr_no
-        curr_no = roman[letter]
-
-       # r += (curr_num if curr_num >= prev_num else -curr_num)
-        if curr_no >= prev_no:
-            r += curr_no
-        else:
-            r += -curr_no
-
-    return r"""
-
+if __name__ == '__main__':
+    tests = [
+        'X',
+        'VII',
+        'IX',
+        'LXXVII',
+        'DCCVII'
+    ]
+    for test in tests:
+        print(test, '=', roman_to_int(test))
